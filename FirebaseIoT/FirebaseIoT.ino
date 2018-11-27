035f901c2989abd29996d838fab58cbde4767622
Sergio Eduardo Pérez Rosales <sergio.eddy25@hotmail.com>
2018-11-26 21:01:25 -0600

    Error
    Error de desbordamiento, hay que cambiar los Dinamyc Buffer

--- FirebaseIoT/FirebaseIoT.ino
+++ FirebaseIoT/FirebaseIoT.ino
@@ -1,15 +1,21 @@
 #include <ESP8266WiFi.h>
 #include <FirebaseArduino.h>
 #include <ArduinoJson.h>
+#include <SoftwareSerial.h>
 
 // Set these to run example.
 #define FIREBASE_HOST "arduino-esp8266-iot.firebaseio.com"  // https://arduino-esp8266-iot.firebaseio.com/
 #define FIREBASE_AUTH "pBPtIFktSZMHQhJABOPCcEL4rhvm9u490zstLCNb"
 #define WIFI_SSID "c6a88a"
 #define WIFI_PASSWORD "280642427"
+//#define WIFI_SSID "LSyC-WIFI"
+//#define WIFI_PASSWORD "a1a2a3itd"
+
+SoftwareSerial serie_esp(0,2);  //(Tx,Rx)
 
 void setup() {
   Serial.begin(9600);
+  serie_esp.begin(9600);
   
   // connect to wifi.
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
@@ -23,24 +29,24 @@ void setup() {
   Serial.println(WiFi.localIP());
   
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-  
 
+  
 }
-  // declare buffer Json 
-  StaticJsonBuffer<200> jsonBuffer;
 
+void loop() {
+  const size_t bufferSize = JSON_OBJECT_SIZE(2) + 30;
+  DynamicJsonBuffer jsonBuffer(bufferSize);
   
-void loop() {    
-  JsonObject& root = jsonBuffer.parseObject(Serial);
-  //JsonObject& root = jsonBuffer.parse(Serial.read());
-  bool led = root["statusLed"];
-  String cadena = root["prueba"];
+  
+  JsonObject& root = jsonBuffer.parseObject(serie_esp);
+  
+  bool statusLed = root["statusLed"]; // true
+  const char* prueba = root["prueba"]; // "Hola"
+    
+    Firebase.setBool("statusLed", statusLed);
+    Firebase.setString("prueba", prueba);
 
-  //root.printTo(Serial);
-  Serial.println(cadena);
-  Serial.println("Prueba");
+    root.printTo(Serial);
 
-  Firebase.setBool("statusLed", led);
-  Firebase.setString("prueba", cadena);
-  delay(1000);
+    //Serial.print((char)serie_esp.read());
 }
--- ModulosRxTx/ModulosRxTx.ino
+++ ModulosRxTx/ModulosRxTx.ino
@@ -1,31 +1,26 @@
 #include <ArduinoJson.h>
+#include <SoftwareSerial.h>
+
+SoftwareSerial serie_virtual(2,3);  //(Tx,Rx)
 
 int pinLED = 8;
 bool statusLed;
-  
-
-StaticJsonBuffer<200> jsonBuffer;
-JsonObject& root = jsonBuffer.createObject();
-
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
-
+  serie_virtual.begin(9600);
+   //Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   
-  statusLed = true;
-  root["statusLed"] = statusLed;
-  root["prueba"] = "Hola";
-  delay(1000);
-  root.printTo(Serial);
+  const size_t bufferSize = JSON_OBJECT_SIZE(2);
+  DynamicJsonBuffer jsonBuffer(bufferSize);
   
-  statusLed = false;
-  root["statusLed"] = statusLed;
-  root["prueba"] = "Adios";
-  delay(1000);
-  root.printTo(Serial);
+  JsonObject& root = jsonBuffer.createObject();
+  root["statusLed"] = true;
+  root["prueba"] = "Hola";
+  
+  root.printTo(serie_virtual);
 }
